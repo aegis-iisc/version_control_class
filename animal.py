9a783fd65dca0eef18c9b686012e3c43b0b9d52a
Ashish Mishra <39313384+aegis-iisc@users.noreply.github.com>
2025-04-02 15:40:57 +0530

    Added the first function

--- animal.py
+++ animal.py
@@ -1,11 +1,16 @@
-
+import sys
 
 
 def default():
     print("Hello to the SDF class")
 
+def first():
+    print("Hello I am the first branch")
 def main():
-    default()
+    if sys.argv[1] == 'first':
+        first()
+    else:    
+        default()
     
 if __name__ == '__main__':
     main()    

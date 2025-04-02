24954455b2b00e739e09ee3f7c7403181208e175
Ashish Mishra <39313384+aegis-iisc@users.noreply.github.com>
2025-04-02 16:09:34 +0530

    committing first after reset

--- animal.py
+++ animal.py
@@ -3,14 +3,18 @@ import sys
 
 def default():
     print("Hello to the SDF class")
-
 def first():
     print("Hello I am the first branch")
+def second():
+    print ("Hello from the second branch")  
 def main():
     if sys.argv[1] == 'first':
         first()
+    elif sys.argv[1] == 'second':
+        second()
     else:    
-        default()
+        default()    
+   
     
 if __name__ == '__main__':
     main()    

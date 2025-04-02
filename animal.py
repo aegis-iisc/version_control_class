3f4550629ea2ea473cd79c7b1d52046d210b6bda
Ashish Mishra <39313384+aegis-iisc@users.noreply.github.com>
2025-04-02 15:49:59 +0530

    second function added

--- animal.py
+++ animal.py
@@ -1,11 +1,15 @@
-
+import sys
 
 
 def default():
     print("Hello to the SDF class")
-
+def second():
+    print ("Hello from the second branch")
 def main():
-    default()
+    if sys.argv[1] == 'second':
+        second()
+    else:    
+        default()
     
 if __name__ == '__main__':
     main()    
